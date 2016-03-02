d3197f47928fb6cdb4d5451951e8c0ccdf236a7f
dev92 <dev92@ccs.neu.edu>
2016-03-01 23:10:48 -0500

    Made changes to LoginCOntroller

--- public/assignment/views/users/login.controller.js
+++ public/assignment/views/users/login.controller.js
@@ -6,14 +6,27 @@
 
     function LoginController($rootScope, $scope, $location, UserService) {
 
-        $scope.dispalert = false;
+        $scope.message = null;
 
         $scope.login = function(user) {
+
+            if (user == null) {
+                $scope.message = "Please Enter Username and Password!";
+                return;
+            }
+            if (user.username == null || !user.username) {
+                $scope.message = "Please enter a username.";
+                return;
+            }
+            if (user.password == null) {
+                $scope.message = "Please enter a password.";
+                return;
+            }
 
             UserService.findUserByCredentials(user.username,user.password,
                 function(response){
                     if(response == null){
-                        $scope.dispalert = true;
+                        $scope.message = "Username/Password does not match!";
                     }else{
                         $rootScope.currentusr = response;
                         $location.path('/profile');
