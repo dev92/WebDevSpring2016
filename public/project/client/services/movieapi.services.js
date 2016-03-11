bcd19b3f478a5fe823b4ef1f6bce6be2da80a075
dev92 <dev92@ccs.neu.edu>
2016-03-11 13:00:11 -0800

    Trying to load images

--- public/project/client/services/movieapi.services.js
+++ public/project/client/services/movieapi.services.js
@@ -6,21 +6,46 @@
 
     function MovieApiService($http) {
 
+        var config = "http://api.themoviedb.org/3/configuration?api_key=c8fee912d3f3866df68026f0ebadc6f6";
+
+        var searchUrl = "http://api.themoviedb.org/3/search/movie?query=QUERY&api_key=c8fee912d3f3866df68026f0ebadc6f6";
+        var genreMovies = "http://api.themoviedb.org/3/genre/GENRE/movies?api_key=c8fee912d3f3866df68026f0ebadc6f6";
+        var detailsUrl = "http://api.themoviedb.org/3/movie/ID?api_key=c8fee912d3f3866df68026f0ebadc6f6";
+
+        $http.get(config).success(function (response) {
+            var path = response;
+            console.log(path);
+        });
+
+        //string.replace(/\\\//g, "/");
+
 
         var api = {
             findMovieByTitle : findMovieByTitle,
-            findMovieByImdbID:findMovieByImdbID
+            findMovieByImdbID:findMovieByImdbID,
+            findBasePath:findBasePath
+            //findImgbyImdbID:findImgbyImdbID
         };
 
         return api;
 
         function findMovieByTitle(title,callback) {
+            //$http.get(searchUrl.replace("QUERY",title))
+            //    .success(callback);
             $http.get("http://www.omdbapi.com/?s="+title)
                 .success(callback);
         }
 
         function findMovieByImdbID(imdbID, callback) {
+            //$http.get(detailsUrl.replace("ID",imdbID))
+            //    .success(callback);
+
             $http.get("http://www.omdbapi.com/?i="+imdbID)
+                .success(callback);
+        }
+
+        function findBasePath(callback){
+            $http.get(config)
                 .success(callback);
         }
 
--- public/project/client/views/search/search.controller.js
+++ public/project/client/views/search/search.controller.js
@@ -7,9 +7,17 @@
 
         $scope.search = search;
         $scope.movietitle = $routeParams.movietitle;
+        //$scope.basepath = null;
 
-        $scope.rate = 3;
-        $scope.max = 5;
+        //$scope.init = function(){
+        //    MovieApiService.findBasePath(function(response){
+        //       $scope.basepath =  response.images.base_url
+        //    });
+        //}
+
+        $scope.imgpath = function (imdbID) {
+            return "http://img.omdbapi.com/?i=ID&apikey=2bf5ee9".replace("ID",imdbID);
+        }
 
 
         if($scope.movietitle) {
@@ -18,12 +26,12 @@
 
         function search(title) {
             $location.url("/search/"+$scope.movietitle);
-            console.log(title);
+            //console.log(title);
             MovieApiService.findMovieByTitle(
                 title,
                 function(response){
-                    console.log(response);
-                    $scope.movies = response;
+                    //console.log(response);
+                    $scope.movies = response.Search;
                 });
         }
     }
