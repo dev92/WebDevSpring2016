72c954de276c35640f1157426994e5209bb8942d
dev92 <dev92@ccs.neu.edu>
2016-03-25 20:21:35 -0400

    made few changes to search and details page

--- public/project/client/views/search/details.controller.js
+++ public/project/client/views/search/details.controller.js
@@ -7,25 +7,22 @@
 
         $scope.imdbID = $routeParams.imdbID;
 
-        //$scope.imgpath = function (imgpath) {
-        //
-        //    //console.log($scope.basepath+"original"+imgpath)
-        //    return $rootScope.basepath+"original"+imgpath;
-        //
-        //    //return "http://img.omdbapi.com/?i=ID&apikey=2bf5ee9".replace("ID",imdbID);
-        //}
-
         $scope.addFavorite = function(movie){
             if($rootScope.currentusr){
                 movie.favorite = !movie.favorite;
             }
         }
 
+        $scope.rateMovie = function(id,rating){
+           //console.log(id+" "+rating);
+        }
+
         MovieApiService.findMovieByImdbID(
             $scope.imdbID,
             function(response) {
                 response.Poster = "http://img.omdbapi.com/?i=ID&apikey=2bf5ee9".replace("ID",response.imdbID)
-                //console.log(response);
+                //console.log((response.imdbRating).toFixed())
+                $scope.rating = Number(response.imdbRating).toFixed();
                 $scope.movie = response;
             }
         )
