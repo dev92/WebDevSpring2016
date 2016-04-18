fbd5cd79fee8cf14228f70c092e60549f7502832
dev92 <dev92@ccs.neu.edu>
2016-04-18 00:32:42 -0400

    fixed db issue on openshift

--- server.js
+++ server.js
@@ -6,24 +6,18 @@ var passport = require('passport');
 var cookieParser = require('cookie-parser');
 var session  = require('express-session');
 var mongoose = require("mongoose");
-var connectionStringAssignment = 'mongodb://127.0.0.1:27017/FormMaker';
-var connectionStringProject = 'mongodb://127.0.0.1:27017/CinePhilia';
+var connectionString = 'mongodb://127.0.0.1:27017/WebDev2016';
 
 if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
-    connectionStringAssignment = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
+    connectionString = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
         process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
         process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
         process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
         process.env.OPENSHIFT_APP_NAME;
 
-    connectionStringProject = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
-        process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
-        process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
-        process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
-        'cinephilia';
 }
-var dbAssignment = mongoose.createConnection(connectionStringAssignment, {server:{poolSize:3}});
-var dbProject = mongoose.createConnection(connectionStringProject, {server:{poolSize:3}});
+var db = mongoose.connect(connectionString);
+
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -45,7 +39,7 @@ app.use(express.static(__dirname + '/public'));
 
 
 //require("./public/assignment/server/app.js")(app,mongoose,dbAssignment);
-require("./public/project/server/app.js")(app,mongoose,dbProject);
+require("./public/project/server/app.js")(app,mongoose,db);
 
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
