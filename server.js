0199f99fae7e5958af86d2c8583cf4710c1edab3
dev92 <dev92@ccs.neu.edu>
2018-05-25 01:05:47 -0700

    modified server.js

--- server.js
+++ server.js
@@ -6,15 +6,13 @@ var passport = require('passport');
 var cookieParser = require('cookie-parser');
 var session  = require('express-session');
 var mongoose = require("mongoose");
-var connectionString = 'mongodb://127.0.0.1:27017/WebDev2016';
+var connectionString = 'mongodb://127.0.0.1:27017/WebDev2016'; // for local
 
-if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
-    connectionString = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
-        process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
-        process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
-        process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
-        process.env.OPENSHIFT_APP_NAME;
-
+if(process.env.MLAB_USERNAME) { // check if running remotely
+    var username = process.env.MLAB_USERNAME; // get from environment
+    var password = process.env.MLAB_PASSWORD;
+    connectionString = 'mongodb://' + username + ':' + password;
+    connectionString += '@ds235860.mlab.com:35860/heroku_9w9phl17';
 }
 var db = mongoose.connect(connectionString);
 
@@ -41,10 +39,9 @@ app.use(express.static(__dirname + '/public'));
 //require("./public/assignment/server/app.js")(app,mongoose,db);
 //require("./public/project/server/app.js")(app,mongoose,db);
 require("./public/app.js")(app,mongoose,db);
-
-var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
-var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
-
+//var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+var port = process.env.PORT || 3000;
 
 
-app.listen(port, ipaddress);
+
+app.listen(port);
